0dbcccd6489e3e4d5844af8c459c88776acb5332
Christoph Wong <wong.christoph@gmail.com>
2017-06-06 16:14:50 +0800

    implement basic board and board checker

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -1,7 +1,34 @@
 (ns tictactoe.core
   (:gen-class))
 
+(defn new-board []
+  [[nil nil nil] [nil nil nil] [nil nil nil]])
+
+(defn get-columns [board]
+  (apply mapv vector board))
+
+(defn get-diagonals [board]
+  (let [get-from-coordinates (fn [coor-vec] (get-in board coor-vec))]
+    [(mapv get-from-coordinates [[0 0] [1 1] [2 2]])
+     (mapv get-from-coordinates [[0 2] [1 1] [2 0]])]))
+
+(defn check-board [board]
+  (let [transpose-board
+        (fn [board]
+          (conj
+            board
+            (get-columns board)
+            (get-diagonals board)))]
+    (first
+      (for [row (transpose-board board)
+            :when (or
+                    (every? #{:x} row)
+                    (every? #{:o} row))
+            :let [winning-mark (first row)]]
+        winning-mark))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
--- test/tictactoe/core_test.clj
+++ test/tictactoe/core_test.clj
@@ -2,6 +2,40 @@
   (:require [clojure.test :refer :all]
             [tictactoe.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest new-board-test
+  (testing "new board creates empty 3 by 3 board"
+    (let [test-board [[nil nil nil] [nil nil nil] [nil nil nil]]]
+      (is (= (new-board) test-board)))))
+
+(deftest transpose-board-test
+  (testing "getting columns from board"
+    (let [test-board [[:x :o :x] [:x :o :x] [:x :o :x]]
+          result [[:x :x :x] [:o :o :o] [:x :x :x]]]
+      (is (= (get-columns test-board) result))))
+
+  (testing "getting diagonals from board"
+    (let [test-board [[:x nil :o] [nil :x nil] [:o nil :x]]
+          result [[:x :x :x] [:o :x :o]]]
+      (is (= (get-diagonals test-board) result)))))
+
+(deftest board-check-test
+  (testing "check board returns nil if no winner"
+    (let [test-board [[nil nil nil] [nil nil nil] [nil nil nil]]]
+      (is (= (check-board test-board) nil)))
+
+    (let [test-board [[:x nil nil] [:x :x :o] [:o nil nil]]]
+      (is (= (check-board test-board) nil))))
+
+  (testing "check board returns winner"
+    (let [winning-board [[:x :x :x] [nil nil nil] [nil nil nil]]]
+      (is (= (check-board winning-board) :x)))
+
+    (let [winning-board [[nil nil nil] [:o :o :o] [nil nil nil]]]
+      (is (= (check-board winning-board) :o))))
+
+  (testing "check board also checks vertical and diagonal winners"
+    (let [winning-board [[:x nil nil] [:x nil nil] [:x nil nil]]]
+      (is (= (check-board winning-board) :x)))
+
+    (let [winning-board [[:o nil nil] [nil :o nil] [nil nil :o]]]
+      (is (= (check-board winning-board) :o)))))
